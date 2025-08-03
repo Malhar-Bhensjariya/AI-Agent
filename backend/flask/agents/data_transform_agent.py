8e9e187de5a7a02d5c8ea9d99e63a0e41d04756e
Malhar-Bhensjariya <mcb110104@gmail.com>
2025-08-03 17:16:15 +0530

    Revert to commit a24f6c4 (UI fix)

--- backend/flask/agents/data_transform_agent.py
+++ backend/flask/agents/data_transform_agent.py
@@ -17,7 +17,7 @@ class DataTransformAgentExecutor:
             raise ValueError("GEMINI_API_KEY not found in environment.")
         
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-pro",
+            model="gemini-2.0-flash",
             google_api_key=api_key,
             temperature=0,
             convert_system_message_to_human=True
--- backend/flask/agents/editor_agent.py
+++ backend/flask/agents/editor_agent.py
@@ -16,7 +16,7 @@ class CSVAgentExecutor:
             raise ValueError("GEMINI_API_KEY not found in environment")
             
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-pro",
+            model="gemini-2.0-flash",
             google_api_key=api_key,
             temperature=0,
             convert_system_message_to_human=True,
